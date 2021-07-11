4a56eedc670ecccd81deec9a6e50d8f34a8cfafa
Juffaz <38725561+juffaz@users.noreply.github.com>
2021-07-11 17:29:46 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,70 @@ require('dotenv').config()
 
 const bot = new Telegraf(process.env.TELETOK)
 
-bot.start((ctx) => ctx.reply('Welcome')) //ответ бота на команду /start
-bot.help((ctx) => ctx.reply('Send me a sticker')) //ответ бота на команду /help
-bot.on('sticker', (ctx) => ctx.reply('')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
-bot.hears('hi', (ctx) => ctx.reply('Hey there')) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
-bot.launch() // запуск бота
+bot.help(ctx => {
+    ctx.reply("The bot can perform the following commands\n - /start\n - /help\n - command")
+})
+bot.command((ctx) => ctx.replyWithPhoto({ source: 'screenshot.png' }))
+bot.launch()
+const puppeteer = require('puppeteer');
+
+const santos_SELECTOR = 'body > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > a:nth-child(1)'
+
+const username_SELECTOR = '#userid';
+
+const password_SELECTOR = '#password';
+
+const cta_SELECTOR = '#btLog'; 
+
+const username = 'user'
+
+const password = 'password'
+
+const movimentacoes = 'div.itPagina:nth-child(30) > a:nth-child(1)'
+
+const url = 'https://www.sppilots.com.br/'
+
+async function startBrowser() {
+
+  const browser = await puppeteer.launch({headless: false});
+
+  const page = await browser.newPage();
+
+  return {browser, page};
+
+}
+
+async function playTest() {
+
+  const {browser, page} = await startBrowser();
+
+  page.setViewport({width: 1366, height: 768});
+
+  await page.goto(url);
+
+  await page.click(santos_SELECTOR)
+
+  await page.click(username_SELECTOR);
+
+  await page.keyboard.type(username);
+
+  await page.click(password_SELECTOR);
+
+  await page.keyboard.type(password);
+
+  await page.click(cta_SELECTOR);
+
+  await page.click(movimentacoes);
+
+  await page.screenshot({path: "screenshot.png"})
+
+}
+
+(async () => {
+
+  await playTest("https://www.sppilots.com.br/");
+
+  process.exit(1);
+
+})();
+
