9ce34240483e319b4715c5ce665ecff612a9c7f0
Juffaz <38725561+juffaz@users.noreply.github.com>
2021-07-11 22:24:44 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,65 +8,14 @@ bot.help(ctx => {
 })
 bot.command((ctx) => ctx.replyWithPhoto({ source: 'screenshot.png' }))
 bot.launch()
+
 const puppeteer = require('puppeteer');
 
-const santos_SELECTOR = 'body > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > a:nth-child(1)'
-
-const username_SELECTOR = '#userid';
-
-const password_SELECTOR = '#password';
-
-const cta_SELECTOR = '#btLog'; 
-
-const username = 'user'
-
-const password = 'password'
-
-const movimentacoes = 'div.itPagina:nth-child(30) > a:nth-child(1)'
-
-const url = 'https://www.sppilots.com.br/'
-
-async function startBrowser() {
-
-  const browser = await puppeteer.launch({headless: false});
-
-  const page = await browser.newPage();
-
-  return {browser, page};
-
-}
-
-async function playTest() {
-
-  const {browser, page} = await startBrowser();
-
-  page.setViewport({width: 1366, height: 768});
-
-  await page.goto(url);
-
-  await page.click(santos_SELECTOR)
-
-  await page.click(username_SELECTOR);
-
-  await page.keyboard.type(username);
-
-  await page.click(password_SELECTOR);
-
-  await page.keyboard.type(password);
-
-  await page.click(cta_SELECTOR);
-
-  await page.click(movimentacoes);
-
-  await page.screenshot({path: "screenshot.png"})
-
-}
-
 (async () => {
-
-  await playTest("https://www.sppilots.com.br/");
-
-  process.exit(1);
-
-})();
-
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1280, height: 800 })
+  await page.goto('https://www.nytimes.com/')
+  await page.screenshot({ path: 'screenshot.png', fullPage: true })
+  await browser.close()
+})()
