075d74bf4134e9e2bf58370b7a8eb1094424a793
Juffaz <38725561+juffaz@users.noreply.github.com>
2021-07-11 22:48:46 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,9 @@ bot.launch()
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+})
   const page = await browser.newPage()
   await page.setViewport({ width: 1280, height: 800 })
   await page.goto('https://www.nytimes.com/')
