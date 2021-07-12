3e0e481e8a7c133fe1c65660d83ad35486014522
Juffaz <38725561+juffaz@users.noreply.github.com>
2021-07-12 11:47:56 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,27 +2,81 @@ const { Telegraf } = require('telegraf')
 require('dotenv').config()
 const bot = new Telegraf(process.env.TELETOK)
 const puppeteer = require('puppeteer');
+const AUTH_DATA = {
+  url:'',
+  login:'',
+  password:''
+}
+const PAGE_URL = 'https://play.grafana.org/d/000000012/grafana-play-home?orgId=1';
 
+const commandArgsMiddleware = () => (ctx, next) => {
+  if (ctx.updateType === 'message' && ctx.updateSubType === 'text') {
+    const text = ctx.update.message.text.toLowerCase()
+    if (text.startsWith('/')) {
+      const match = text.match(/^\/([^\s]+)\s?(.+)?/)
+      let args = []
+      let command
+      if (match !== null) {
+        if (match[1]) {
+          command = match[1]
+        }
+        if (match[2]) {
+          args = match[2].split(' ')
+        }
+      }
 
-(async () => {
+      ctx.state.command = {
+        raw: text,
+        command,
+        args
+      }
+    }
+  }
+  return next()
+}
+
+async function initBrowser() {
   const browser = await puppeteer.launch({
-  args: ['--no-sandbox', '--disable-setuid-sandbox'],
-})
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  })
   const page = await browser.newPage()
   await page.setViewport({ width: 1280, height: 800 })
-  await page.goto('https://play.grafana.org/d/000000012/grafana-play-home?orgId=1')
-  await page.waitForNavigation({waitUntil: 'networkidle2'});
+  return { browser, page };
+}
+
+const loginGrapha = async() => {
+  const { browser, page } = await initBrowser();
+  const { url, login, password } = AUTH_DATA;
+  await page.goto(url);
+  await page.click('.loginInputFieldclass');
+  await page.keyboard.type(login);
+  await page.click('.passwordInputFieldclass');
+  await page.keyboard.type(password);
+  await page.click('.loginButton');
+  return { browser, page };
+}
+
+
+const getScreenShot = async (url) => {
+  const {browser, page} = await initBrowser();
+  await page.goto(url, {"waitUntil" : "domcontentloaded"});
   await page.screenshot({ path: 'screenshot.png', fullPage: true })
-  await browser.close()
-  
-  bot.help(ctx => {
+  await browser.close();
+});
+
+
+bot.use(commandArgsMiddleware());
+
+bot.help(ctx => {
     ctx.reply("The bot can perform the following commands\n - /start\n - /help\n - command")
-   })
-  bot.command((ctx) => ctx.replyWithPhoto({ source: 'screenshot.png' }))
-  bot.launch()
-  
-  })()
+})
 
+bot.command(async (ctx) => {
+  await getScreenShot(PAGE_URL);
+  console.log(ctx.state.command);
+  ctx.replyWithPhoto({ source: 'screenshot.png' });
+});
 
+bot.launch()
 
 
